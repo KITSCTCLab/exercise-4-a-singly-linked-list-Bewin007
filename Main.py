564d65154051f059c25d8efedc027e2f2534771f
Bewin Felix <99880789+Bewin007@users.noreply.github.com>
2022-09-10 20:21:29 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,86 +2,116 @@ from typing import Optional
 
 
 class Node:
-  def __init__(self,data):
-    self.data = data
-    self.next = None
+    """
+    This class describes Node objects to act as elements of the LinkedList
+    Attributes:
+        -> data - stored associated data
+        -> next - link to next node
+    """
 
-class Link:
-  def __init__(self):
-    self.head = None
-    self.count = 0
-  
-  def push(self,data):
-    new = Node(data)
-    new.next = self.head
-    self.head = new
-    self.count += 1
-  
-  def search(self,target):
-    n = self.head
-    result = 0
-    node_number = 0
-    new = {}
-    for i in range(self.count):
-      if(n.data == target):
-        new = {
-            "postion:":i,
-            "node value:":target
-        }
-    print()
-    return new
+    def __init__(self, data=None, next=None):
+        """
+        Initialises the Node with given attributes
+        """
+        self.data = data
+        self.next = next
 
+
+class LinkedList:
+    """
+    This class implements LinkedList using Node objects
+    Methods:
+        -> insert_at_end - inserts node with data at the end of the list
+        -> status - displays all elements of the lisT
+    Attributes
+        -> self.head - contains first node of LinkedList, None if list empty
+    """
+
+    def __init__(self):
+        """
+        Initialize the head
+        """
+        self.head = None
+
+    def insert_at_end(self, data):
+        """
+        Insert node at end of the list
+        :param data: integer data that will be used to create a node
+        """
+        new = Node(data, None)
+        curr = self.head
+        if curr is None:
+            self.head = new
+        else:
+            while curr.next is not None:
+                curr = curr.next
+            curr.next = new
+
+    def status(self):
+        """
+        It prints all the elements of list.
+        """
+        elements = []
+        curr = self.head
+        while curr:
+            elements.append(curr.data)
+            curr = curr.next
+        print(elements)
+
+
+class Solution:
+    """
+    Class implementing functions to add numbers in a LinkedListT
+    
+    """
+
+    def addTwoNumbers(self, first_list: Optional[LinkedList], second_list: Optional[LinkedList]) -> Optional[
+        LinkedList]:
+        """
+        :param first_list: Linkedlist with non-negative integers
+        :param second_list: Linkedlist with non-negative integers
+        :return: returns the sum as a linked list
+        """
+        result = self.get_num(first_list) + self.get_num(second_list)
+        sum_list = LinkedList()
+        for digit in list(map(int, str(result)[::-1])):
+            sum_list.insert_at_end(digit)
+        return sum_list
+
+    def get_num(self, l: Optional[LinkedList]) -> int:
+        """
+        :param l: LinkedList with non-negative integers
+        :return: returns digits of the list as a single integer
+        """
+        curr = l.head
+        if curr is None:
+            return 0
+        num = ""
+        while curr is not None:
+            num = str(curr.data) + num
+            curr = curr.next
+        return int(num)
+        
         
 
-  def pair(self):
-    n = self.head
-    while(n):
-      print(n.data,end = "->")
-      n = n.next
-    print("\n")
-    
-  def delete_beg(self):
-    temp = self.head
-    self.head = self.head.next
-    temp = None
-  
-  def delete_end(self):
-    for i in range(self.count):
-      if(i == self.count - 1):
-        temp = self.head
-        self.head.next = None
-        temp = None
-
-
-  def reverse(self):
-    curr , prev , next = self.head , None,None
-    while(curr != None):
-      next = curr.next
-      curr.next = prev
-      prev = curr
-      curr = next
-    self.head = prev
-
-l1 = Link()
-# while(1):
-#   x = int(input())
-#   if(x == 1):
-#     val = int(input())
-#     l1.push(val)
-#   elif(x == 2):
-#     l1.reverse()
-#   elif(x == 3):
-#     l1.pair()
-#   elif(x == 4):
-#     search = int(input())
-#     print(l1.search(search))
-
-for i in range(3):
-  val = int(input())
-  l1.push(val)
-
-l1.pair()
-l1.delete_beg()
-l1.pair()
-l1.delete_end()
-l1.pair()
+# Do not edit the following code      
+# Create an instance for LinkedList
+first_list = LinkedList()
+# Create an another instance for LinkedList
+second_list = LinkedList()
+# Read data for first list
+data_for_first_list = list(map(int, input().strip().split(" ")))
+# Add data at the end of first_list
+for data in data_for_first_list:
+    first_list.insert_at_end(data)
+# Read data for second list
+data_for_second_list = list(map(int, input().strip().split(" ")))
+# Add data at the end of second_list
+for data in data_for_second_list:
+    second_list.insert_at_end(data)
+# Create an instance for Solution
+solution = Solution()
+# Pass first_list and second_list to addTwoNumbers, which returns a new linked list
+new_list = solution.addTwoNumbers(first_list, second_list)
+# Display the status of new_list
+new_list.status()
